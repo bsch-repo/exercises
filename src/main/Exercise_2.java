902ff7e4f2a514f65499fe2c74401395f4be1e21
bsch <bartlomiej.schneider@gmail.com>
2018-11-27 16:48:17 +0000

    exercise with reading and adding binary numbers

--- src/main/Exercise_2.java
+++ src/main/Exercise_2.java
@@ -1,34 +1,24 @@
 package main;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Exercise_2 {
 
-    static int x = 1;
-    static int y = 2;
-    static int buffer_1 = 0;
-    static int buffer_2 = 0;
+    static void toBinary() throws InputMismatchException {
 
-    static void printVariables(String type) {
-        switch (type) {
-            case "unswapped":
-                System.out.println("Unswapped variables: " + x + " " + y);
-                break;
-            case "swapped":
-                System.out.println("Swapped variables: " + x + " " + y);
-                break;
-        }
+        Scanner reader = new Scanner(System.in);
+        reader.useRadix(2);
 
-    }
+        long a = reader.nextLong();
+        long b = reader.nextLong();
+        long result = a+b;
 
-    static void swapVariables() {
-        buffer_1 = y;
-        buffer_2 = x;
-        x = buffer_1;
-        y = buffer_2;
+        System.out.println(Long.toBinaryString(result));
+
     }
 
     public static void main(String[] args) {
-        printVariables("unswapped");
-        swapVariables();
-        printVariables("swapped");
+        toBinary();
     }
 }
