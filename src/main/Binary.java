170df43bf4ffb71b1023ed3e27393261a77ff63a
bsch <bartlomiej.schneider@gmail.com>
2018-12-12 15:28:21 +0000

    added partial multiplication algorithm

--- src/main/Binary.java
+++ src/main/Binary.java
@@ -34,6 +34,25 @@ public class Binary {
 
     }
 
+    public static int getBiggerBinary(ArrayList<Integer> BinaryA, ArrayList<Integer> BinaryB) {
+        int biggerBinary = 0;
+
+        // check which binary number is smaller and fill smaller with 0
+        if (BinaryA.size() > BinaryB.size()) {
+            biggerBinary = BinaryA.size();
+            for (int i = (BinaryA.size() - (BinaryA.size()- BinaryB.size())); i < BinaryA.size(); i++) {
+                BinaryB.add(i, 0);
+            }
+        } else {
+            biggerBinary = BinaryB.size();
+            for (int i = (BinaryB.size() - (BinaryB.size()- BinaryA.size())); i < BinaryB.size(); i++) {
+                BinaryA.add(i, 0);
+            }
+        }
+
+        return biggerBinary;
+    }
+
     public static void addBinary(ArrayList<Integer> BinaryA, ArrayList<Integer> BinaryB, ArrayList<Integer> targetBinary) {
         boolean carryFlag = false;
 
@@ -61,27 +80,25 @@ public class Binary {
         displayBinary(resultBinary);
     }
 
-    public static int getBiggerBinary(ArrayList<Integer> BinaryA, ArrayList<Integer> BinaryB) {
-        int biggerBinary = 0;
+    public static void multiplybinary(ArrayList<Integer> multipliedBinary, ArrayList<Integer> multiplier, ArrayList<Integer> targetBinary) {
+        ArrayList<Integer> interSum = new ArrayList<>();
 
-        // check which binary number is smaller and fill smaller with 0
-        if (BinaryA.size() > BinaryB.size()) {
-            biggerBinary = BinaryA.size();
-            for (int i = (BinaryA.size() - (BinaryA.size()- BinaryB.size())); i < BinaryA.size(); i++) {
-                BinaryB.add(i, 0);
+        for (int i = 0; i < Binary.getBiggerBinary(multipliedBinary, multiplier); i++) {
+
+            for (int j = 0; j < multiplier.size(); j++) {
+                switch (multipliedBinary.get(i) * multiplier.get(i)) {
+                    case 0:
+                        interSum.add(0);
+                        break;
+                    case 1:
+                        interSum.add(1);
+                        break;
+                }
             }
-        } else {
-            biggerBinary = BinaryB.size();
-            for (int i = (BinaryB.size() - (BinaryB.size()- BinaryA.size())); i < BinaryB.size(); i++) {
-                BinaryA.add(i, 0);
-            }
+
         }
 
-        return biggerBinary;
-    }
-
-    public static void multiplybinary(ArrayList<Integer> BinaryA, ArrayList<Integer> BinaryB, ArrayList<Integer> targetBinary) {
-
+        displayBinary(resultBinary);
     }
 
 }
